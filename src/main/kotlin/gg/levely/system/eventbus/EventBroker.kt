04f5c6ab769c3dcab6866f7f847d1de60455958e
RealAlphaUA <realalpha.ua@gmail.com>
2025-12-30 17:47:31 +0100

    Refactor EventBus to implement BranchCreator and EventBroker, enhancing event subscription and branching capabilities

---
+++ src/main/kotlin/gg/levely/system/eventbus/EventBroker.kt
@@ -0,0 +1,37 @@
+package gg.levely.system.eventbus
+
+import gg.levely.system.eventbus.context.EventContext
+
+abstract class EventBroker<T> {
+
+    abstract fun <E : T> subscribe(
+        eventType: Class<E>,
+        listener: EventListener<E>,
+        eventPriority: EventPriority,
+        eventFilter: EventFilter<E>
+    ): EventContext<E>
+
+    fun <E : T> subscribe(eventType: Class<E>, listener: EventListener<E>): EventContext<E> {
+        return subscribe(eventType, listener, EventPriority.NORMAL, EventFilters.exact())
+    }
+
+    fun <E : T> subscribe(eventType: Class<E>, listener: EventListener<E>, priority: EventPriority) : EventContext<E> {
+        return subscribe(eventType, listener, priority, EventFilters.exact())
+    }
+
+    inline fun <reified E : T> subscribe(
+        priority: EventPriority = EventPriority.NORMAL,
+        filter: EventFilter<E> = EventFilters.exact(),
+        listener: EventListener<E>
+    ) = subscribe(E::class.java, listener, priority, filter)
+
+    inline fun <reified E : T> subscribe(
+        listener: EventListener<E>,
+        priority: EventPriority = EventPriority.NORMAL,
+        filter: EventFilter<E> = EventFilters.exact()
+    ) = subscribe(E::class.java, listener, priority, filter)
+
+    abstract fun <E : T> publish(event: E): E
+
+}
+
--- src/main/kotlin/gg/levely/system/eventbus/EventBus.kt
+++ src/main/kotlin/gg/levely/system/eventbus/EventBus.kt
@@ -4,11 +4,7 @@ import gg.levely.system.eventbus.context.DefaultEventContext
 import gg.levely.system.eventbus.context.EventContext
 import gg.levely.system.eventbus.logger.DebugLogger
 import gg.levely.system.eventbus.logger.EventType
-import kotlinx.coroutines.*
-import java.io.Closeable
-import java.util.UUID
 import java.util.concurrent.ConcurrentSkipListSet
-import kotlin.coroutines.CoroutineContext
 
 /**
  * Event bus that manages listeners and allows triggering custom events.
@@ -18,7 +14,7 @@ import kotlin.coroutines.CoroutineContext
  */
 class EventBus<T>(
     private val enableLogger: Boolean = false
-) {
+) : BranchCreator<T>, EventBroker<T>() {
 
     /**
      * The set of registered event contexts.
@@ -28,141 +24,45 @@ class EventBus<T>(
             .thenBy { it.hashCode() }
     )
 
+    private val children = mutableListOf<EventBranch<T>>()
+
     /**
      * The debug logger instance.
      */
     private val logger by lazy { DebugLogger() }
 
     /**
-     * The root event scope.
-     */
-    private val rootScope = EventScope(name = "root") { ctx -> eventContexts.remove(ctx) }
-
-    /**
-     * Creates a new event scope.
-     *
-     * @param name The name of the scope
-     * @return The created [EventScope]
-     */
-    @JvmOverloads
-    fun createScope(name: String = UUID.randomUUID().toString()): EventScope = rootScope.createScope(name)
-
-    /**
-     * DSL-style scoped block.
-     *
-     * @param name The name of the scope
-     * @param block The block to execute within the scope
-     * @return The created [EventScope]
-     */
-    @JvmOverloads
-    inline fun scoped(name: String = UUID.randomUUID().toString(), block: SubscribeContext<T>.() -> Unit): EventScope {
-        val scope = createScope(name)
-        SubscribeContext(this, scope).apply(block)
-        return scope
-    }
-
-    /**
      * Subscribes to an event of type [E].
      *
      * @param E The event type
-     * @param clazz The class of the event type
+     * @param eventType The class of the event type
      * @param listener The event listener
-     * @param priority The priority of the event listener
-     * @param filter The filter for the event listener
+     * @param eventPriority The priority of the event listener
+     * @param eventFilter The filter for the event listener
      */
-    @JvmOverloads
-    fun <E : T> subscribe(
-        clazz: Class<E>,
+    override fun <E : T> subscribe(
+        eventType: Class<E>,
         listener: EventListener<E>,
-        priority: EventPriority = EventPriorities.NORMAL,
-        filter: EventFilter = EventFilter.ONLY
-    ) {
-        val context = DefaultEventContext(clazz, listener).apply {
-            withEventFilter(filter)
-            withEventPriority(priority)
+        eventPriority: EventPriority,
+        eventFilter: EventFilter<E>
+    ): EventContext<E> {
+        val context = DefaultEventContext(eventType, listener).apply {
+            withEventFilter(eventFilter)
+            withEventPriority(eventPriority)
         }
         eventContexts.add(context)
         if (enableLogger) {
-            logger.logEvent(EventType.SUBSCRIBE, clazz, listener)
+            logger.logEvent(EventType.SUBSCRIBE, eventType, listener)
         }
+        return context
     }
 
-    /**
-     * Subscribes to an event of type [E].
-     *
-     * @param E The event type
-     * @param priority The priority of the event listener
-     * @param filter The filter for the event listener
-     * @param listener The event listener
-     */
-    inline fun <reified E : T> subscribe(
-        priority: EventPriority = EventPriorities.NORMAL,
-        filter: EventFilter = EventFilter.ONLY,
-        listener: EventListener<E>
-    ) = subscribe(E::class.java, listener, priority, filter)
-
-    /**
-     * Subscribes to an event of type [E].
-     *
-     * @param E The event type
-     * @param listener The event listener
-     * @param priority The priority of the event listener
-     * @param filter The filter for the event listener
-     */
-    inline fun <reified E : T> subscribe(
-        listener: EventListener<E>,
-        priority: EventPriority = EventPriorities.NORMAL,
-        filter: EventFilter = EventFilter.ONLY
-    ) = subscribe(E::class.java, listener, priority, filter)
-
-    /**
-     * Subscribes to an event of type [E] within a specific [scope].
-     *
-     * @param E The event type
-     * @param scope The event scope
-     * @param clazz The class of the event type
-     * @param listener The event listener
-     * @param priority The priority of the event listener
-     * @param filter The filter for the event listener
-     * @return A [Subscription] that can be used to unsubscribe
-     */
-    @JvmOverloads
-    fun <E : T> subscribe(
-        scope: EventScope,
-        clazz: Class<E>,
-        listener: EventListener<E>,
-        priority: EventPriority = EventPriorities.NORMAL,
-        filter: EventFilter = EventFilter.ONLY
-    ): Subscription {
-        val context = DefaultEventContext(clazz, listener).apply {
-            withEventFilter(filter)
-            withEventPriority(priority)
-        }
+    internal fun subscribe(context: EventContext<Any>) {
         eventContexts.add(context)
         if (enableLogger) {
-            logger.logEvent(EventType.SUBSCRIBE, clazz, listener)
+            logger.logEvent(EventType.SUBSCRIBE, context.eventType, context.eventListener)
         }
-        return scope.track(context)
     }
-
-    /**
-     * Subscribes to an event of type [E] within a specific [scope].
-     *
-     * @param E The event type
-     * @param scope The event scope
-     * @param priority The priority of the event listener
-     * @param filter The filter for the event listener
-     * @param listener The event listener
-     * @return A [Subscription] that can be used to unsubscribe
-     */
-    @JvmOverloads
-    inline fun <reified E : T> subscribe(
-        scope: EventScope,
-        priority: EventPriority = EventPriorities.NORMAL,
-        filter: EventFilter = EventFilter.ONLY,
-        listener: EventListener<E>
-    ): Subscription = subscribe(scope, E::class.java, listener, priority, filter)
-
 
     /**
      * Unsubscribes from an event of type [E].
@@ -189,18 +89,33 @@ class EventBus<T>(
     }
 
     /**
+     * Unsubscribes using the provided event context.
+     *
+     * @param context The event context to unsubscribe
+     */
+    fun unsubscribe(context: EventContext<Any>) {
+        eventContexts.remove(context)
+        if (enableLogger) {
+            logger.logEvent(EventType.UNSUBSCRIBE, context.eventType, context.eventListener)
+        }
+    }
+
+    /**
      * Publishes an event to all matching listeners.
      *
      * @param E The event type
      * @param event The event to publish
      * @return The published event
      */
-    fun <E : T> publish(event: E): E {
+    override fun <E : T> publish(event: E): E {
         val eventClass = event!!::class.java
 
         eventContexts
             .asSequence()
-            .filter { ctx -> matches(ctx.eventType, eventClass, ctx.eventFilter) }
+            .filter { ctx ->
+                val normalizedEventFilter = ctx.eventFilter as EventFilter<Any>
+                normalizedEventFilter.test(event, ctx.eventType as Class<Any>)
+            }
             .forEach { ctx ->
                 @Suppress("UNCHECKED_CAST")
                 val listener = ctx.eventListener as EventListener<E>
@@ -215,18 +130,10 @@ class EventBus<T>(
         return event
     }
 
-    /**
-     * Checks if the subscriber type matches the event type based on the filter.
-     *
-     * @param subscriberType The subscriber event type
-     * @param eventType The published event type
-     * @param filter The event filter
-     * @return True if it matches, false otherwise
-     */
-    private fun matches(subscriberType: Class<*>, eventType: Class<*>, filter: EventFilter): Boolean {
-        return when (filter) {
-            EventFilter.ONLY -> subscriberType == eventType
-            else -> subscriberType.isAssignableFrom(eventType)
-        }
+    override fun branch(name: String): EventBranch<T> {
+        val childBranch = DefaultEventBranch(name, this, null)
+        children.add(childBranch)
+        return childBranch
     }
+
 }
